35c42efeb5373867638c683d73903107d49cc0a5
JulienBe <bertozzijulien@gmail.com>
2020-05-03 11:59:00 +0200

    app state

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
 
-#[get("/{id}/{name}/index.html")]
-async fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+struct AppState {
+    app_name: String,
 }
 
+// A request handler is an async function that accepts zero or more parameters that can be extracted from a request (ie, impl FromRequest) and returns a type that can be converted into an HttpResponse (ie, impl Responder):
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {}", app_name)
+}
+
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {    
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .data(AppState {
+                app_name: String::from("Chaoss"),
+            })
+            .route("/hello/", web::get().to(greet))
+            .route("/hello/{name}", web::get().to(greet))
+            .route("/name", web::get().to(index))
     })
         .bind("127.0.0.1:8080")?
         .run()
